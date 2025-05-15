53367bc7e4ac30f60b53bc165e45393704f1180e
Ace0816 <2407008@stu.chi.ac.uk>
2025-05-15 17:56:47 +0100

    Enemy AI script created and cached refs to playe and enemy pos, plus hooked them up in the inspector

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //ref to players position
+    [SerializeField] Transform target;
+    //ref to enemy position
+    [SerializeField] NavMeshAgent nMA;
+
+    private void Update()
+    {
+        
+    }
+}
