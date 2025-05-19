1f7ca0420e4cb061ec4a36f6e8dbb54d046991c4
Ace0816 <2407008@stu.chi.ac.uk>
2025-05-19 11:20:39 +0100

    Added in the axe and created a script to be able to pick the axe up

---
+++ Assets/Scripts/WeaponPickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+    public GameObject weapon;
+    public GameObject weaponToDestroy;
+    private bool canPickUp = false;
+
+    private void Update()
+    {
+        if(canPickUp && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(weaponToDestroy);
+            weapon.SetActive(true);
+            canPickUp = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Weapon")
+        {
+            weaponToDestroy = other.gameObject;
+            canPickUp = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            canPickUp = false;
+            weaponToDestroy = null;
+        }
+    }
+}
