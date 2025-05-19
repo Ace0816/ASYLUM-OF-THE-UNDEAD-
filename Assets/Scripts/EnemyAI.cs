5c3a1cb63e84105b106eedfe2d195fc808aea224
Ace0816 <2407008@stu.chi.ac.uk>
2025-05-19 09:36:08 +0100

    Set up the aggro range for the enemy so that he only moves when the player enters the chase range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,13 +11,21 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] NavMeshAgent nMA;
 
     //parameters
-    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float chaseRange = 3.5f;
     float distanceToTarget;
 
     private void Update()
     {
-        //uses the NavMeshAgent Variable to update the destination to the players position, allowing the NMA to be able to calcuate a new path.
-        nMA.SetDestination(target.transform.position);
+        //uses that chase range and distance to target parameter to set up that the enemy will only begin moving once the player enters that chase range
+
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        //This method calculates the shortest distance between the two input points
+
+        if (distanceToTarget <= chaseRange)
+        {
+            nMA.SetDestination(target.transform.position);
+            //uses the NavMeshAgent Variable to update the destination to the players position, allowing the NMA to be able to calcuate a new path.
+        }
     }
 
     private void OnDrawGizmosSelected()
