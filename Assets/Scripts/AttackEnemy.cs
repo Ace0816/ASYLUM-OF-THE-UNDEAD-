06deadec24e4619de22eab8dfc5c95553898b322
Ace0816 <2407008@stu.chi.ac.uk>
2025-05-20 12:05:26 +0100

    set up code so that the axe swings when the left mouse buton is pressed and added a delay to how often you can click to play the animation

---
+++ Assets/Scripts/AttackEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEnemy : MonoBehaviour
+{
+    //weapon animation code
+    private Animator anim;
+    public float clickCooldown = 1f;
+    private float nextAllowedClickTime = 0f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextAllowedClickTime)
+        {
+            anim.Play("Swing", 0, 0f);
+            nextAllowedClickTime = Time.time + clickCooldown;
+        }
+    }
+
+
+}
