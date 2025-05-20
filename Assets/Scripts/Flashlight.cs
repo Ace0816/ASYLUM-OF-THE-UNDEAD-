7285d286edadb9bd2c2eb5ed094f4e7de8fd41e5
Ace0816 <2407008@stu.chi.ac.uk>
2025-05-20 16:37:09 +0100

    Added a script for the flashlight to be able to toggle it on or off

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light Spotlight;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("shift key pressed!");
+
+            if(Spotlight != null)
+            {
+                Spotlight.enabled = !Spotlight.enabled;
+                Debug.Log("spotlight toggled: " + Spotlight.enabled);
+            }
+            else
+            {
+                Debug.LogWarning("Target light not assigned");
+            }
+        }
+    }
+}
